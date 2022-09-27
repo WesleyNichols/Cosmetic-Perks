ef37d4c7fd3a3c14c592d8eecd493b65c9ae014b
TheSheepGoesBa <ericbrewster49@gmail.com>
2022-09-27 00:31:28 -0700

    Added Explanations

--- src/main/java/cosmetic/perks/cosmeticperks/structures/Methods.java
+++ src/main/java/cosmetic/perks/cosmeticperks/structures/Methods.java
@@ -21,28 +21,63 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 
 public abstract class Methods {
-    public Enchantment[] enchantArray(Enchantment... e) {
-        return e;
+
+    /**
+     * Returns an array of enchantments
+     *
+     * @param enchants Enchants
+     */
+    public Enchantment[] enchantArray(Enchantment... enchants) {
+        return enchants;
     }
 
+    /**
+     * Returns an array of integers for the enchant levels
+     *
+     * @param i Integers
+     */
     public int[] levelArray(int... i) {
         return i;
     }
 
+    /**
+     * Sets and stores the player's active trail
+     *
+     * @param e Enum for the trail
+     * @param player The player to set the trail for
+     * @param key The group of the trail
+     */
     public void setActiveTrail(PlayerTrails e, Player player, String key) {
         player.getPersistentDataContainer().set(new NamespacedKey(CosmeticPerks.getInstance(), key + "-trail"), PersistentDataType.STRING, e.name().toUpperCase());
         player.sendMessage(Component.text("Enabled the " + e.getEffectName() + " " + key + " trail!"));
     }
 
+    /**
+     * Removes the player's active trail
+     *
+     * @param player The player to remove the trail for
+     * @param key The group of the trail
+     */
     public void removeActiveTrail(Player player, String key) {
         player.getPersistentDataContainer().set(new NamespacedKey(CosmeticPerks.getInstance(), key + "-trail"), PersistentDataType.STRING, "NONE");
         player.sendMessage(Component.text("Disabled your " + key + " trail!"));
     }
 
+    /**
+     * Gets the player's active trail
+     *
+     * @param player The player to get the active trail for
+     * @param key The group of the trail
+     */
     public String getActiveTrail(Player player, String key) {
         return player.getPersistentDataContainer().get(new NamespacedKey(CosmeticPerks.getInstance(), key + "-trail"), PersistentDataType.STRING);
     }
 
+    /**
+     * Enables an item in the GUI(enchants it)
+     *
+     * @param item The GuiItem to enchant
+     */
     public void enableItem(GuiItem item) {
         ItemMeta itemMeta = item.getItem().getItemMeta();
         itemMeta.addEnchant(Enchantment.DURABILITY, 1, true);
@@ -50,6 +85,11 @@ public abstract class Methods {
         item.getItem().setItemMeta(itemMeta);
     }
 
+    /**
+     * Disables an item in the GUI(disenchants it)
+     *
+     * @param item The GuiItem to disenchant
+     */
     public GuiItem disableItem(GuiItem item) {
         ItemMeta itemMeta = item.getItem().getItemMeta();
         itemMeta.removeEnchant(Enchantment.DURABILITY);
