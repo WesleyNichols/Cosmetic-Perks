deb21a5de6f48fc523fa0a9e4a396a8065a76f83
TheSheepGoesBa <ericbrewster49@gmail.com>
2023-01-09 23:42:34 -0800

    Cleanup array init

--- src/main/java/cosmetic/perks/cosmeticperks/util/AnimationValueInitialize.java
+++ src/main/java/cosmetic/perks/cosmeticperks/util/AnimationValueInitialize.java
@@ -3,10 +3,14 @@ package cosmetic.perks.cosmeticperks.util;
 import cosmetic.perks.cosmeticperks.managers.AnimationValueManager;
 import cosmetic.perks.cosmeticperks.structures.AnimationValues;
 import cosmetic.perks.cosmeticperks.structures.Animations;
+import net.kyori.adventure.text.Component;
 import net.objecthunter.exp4j.ExpressionBuilder;
+import org.bukkit.Bukkit;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AnimationValueInitialize {
     private final String Name;
@@ -39,33 +43,34 @@ public class AnimationValueInitialize {
     }
 
     private double[][][] generateEquationValueList() {
+        //No animations present
         if(this.Animation == null) {return new double[][][]{};}
-        int finalListLength = 0;
-        double[][][][] storedValues = new double[Animation.length][][][];
-        for(int a = 0; a < Animation.length; a++) {
-            String[] equationList = Animation[a].getEquationList();
-            double[] offset = Animation[a].getOffset();
-            finalListLength += equationList.length/3;
-            int length = Animation[a].getTickToComplete() * (Animation[a].isReversingAnimation() ? 2 : 1);
-            double[][][] tempList = new double[equationList.length/3][length][3];
+        //Equation Value List
+        List<double[][]> storedEValues = new ArrayList<>();
+        for(Animations animation: Animation) {
+            String[] equationList = animation.getEquationList();
+            double[] offset = animation.getOffset();
+            int length = animation.getTickToComplete() * (animation.isReversingAnimation() ? 2 : 1);
             int exprNumber;
             for (int i=0; i<equationList.length/3; i++) {
+                //Equating the equation
+                double[][] temp = new double[length][3];
                 for(int j=0; j<length; j++) {
                     exprNumber = (i) * 3;
-                    Animation[a].addToCurrentDistance();
+                    animation.addToCurrentDistance();
                     for (int k = 0; k<3; k++) {
-                        tempList[i][j][k] = BigDecimal.valueOf(new ExpressionBuilder(equationList[exprNumber+k]).variable("x").build().setVariable("x", Animation[a].getCurrentDistance()).evaluate() + offset[k]).setScale(5, RoundingMode.UP).doubleValue();
+                        temp[j][k] = BigDecimal.valueOf(new ExpressionBuilder(equationList[exprNumber+k]).variable("x").build().setVariable("x", animation.getCurrentDistance()).evaluate() + offset[k]).setScale(5, RoundingMode.UP).doubleValue();
                     }
                 }
+                storedEValues.add(temp);
+                }
             }
-            storedValues[a] = tempList;
+
+        double[][][] finalList = new double[storedEValues.size()][][];
+        for(int c = 0; c < storedEValues.size(); c++) {
+            finalList[c] = storedEValues.get(c);
         }
-        double[][][] finalList = new double[finalListLength][][];
-        for (double[][][] storedValue : storedValues) {
-            for (int c = 0; c < storedValue.length; c++) {
-                finalList[c] = storedValue[c];
-            }
-        }
+
         return finalList;
     }
 }
