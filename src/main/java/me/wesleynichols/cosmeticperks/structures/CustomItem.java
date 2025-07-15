d57396fc4fb664224fb9ccafcb6a54d8a7dcb7a1
Wesley Nichols <60758454+WesleyNichols@users.noreply.github.com>
2025-07-15 02:03:09 -0700

    Update CustomItem.java

--- src/main/java/me/wesleynichols/cosmeticperks/structures/CustomItem.java
+++ src/main/java/me/wesleynichols/cosmeticperks/structures/CustomItem.java
@@ -96,7 +96,7 @@ public class CustomItem {
                 LOGGER.warning("Failed to set leather armor color - " + e.getMessage());
             }
         }
-        
+
         itemMeta.addItemFlags(
                 ItemFlag.HIDE_ADDITIONAL_TOOLTIP,
                 ItemFlag.HIDE_ARMOR_TRIM,
